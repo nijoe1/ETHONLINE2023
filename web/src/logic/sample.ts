e9d3bec0713f7088575c83bd658ef1b0dab783ab
nijoe1 <nicklioniss@gmail.com>
2023-10-14 16:13:33 +0300

    testing prod

--- web/src/logic/sample.ts
+++ web/src/logic/sample.ts
@@ -1,148 +1,174 @@
-import { ethers, getAddress, ZeroAddress } from "ethers"
+import { ethers, getAddress, ZeroAddress } from "ethers";
 import { getProvider } from "./web3";
-import { GelatoRelay } from "@gelatonetwork/relay-sdk"
+import { GelatoRelay } from "@gelatonetwork/relay-sdk";
 import { submitTxs } from "./safeapp";
 import { getManager } from "./protocol";
 import { getCurrentNonce } from "./safe";
-import { getSafeMultisigTxs, SafeMultisigTransaction, SafeTransaction } from "./services";
+import {
+  getSafeMultisigTxs,
+  SafeMultisigTransaction,
+  SafeTransaction,
+} from "./services";
 
-const SAMPLE_PLUGIN_CHAIN_ID = 5
-const SAMPLE_PLUGIN_ADDRESS = getAddress("0x59C6f5Ca28E14aA165A0f0145FB1a47011F44Cf9")
-export const NATIVE_TOKEN = getAddress("0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE");
+const SAMPLE_PLUGIN_CHAIN_ID = 5;
+const SAMPLE_PLUGIN_ADDRESS = getAddress(
+  "0x59C6f5Ca28E14aA165A0f0145FB1a47011F44Cf9"
+);
+export const NATIVE_TOKEN = getAddress(
+  "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
+);
 const SAMPLE_PLUGIN_ABI = [
-    "function maxFeePerToken(address account, address token) public view returns (uint256 maxFee)",
-    "function setMaxFeePerToken(address token, uint256 maxFee) external",
-    "function executeFromPlugin(ISafeProtocolManager manager, ISafe safe, SafeTransaction calldata safetx) external"
-]
+  "function maxFeePerToken(address account, address token) public view returns (uint256 maxFee)",
+  "function setMaxFeePerToken(address token, uint256 maxFee) external",
+  "function executeFromPlugin(ISafeProtocolManager manager, ISafe safe, SafeTransaction calldata safetx) external",
+];
 const ECR20_ABI = [
-    "function decimals() public view returns (uint256 decimals)",
-    "function symbol() public view returns (string symbol)",
-]
+  "function decimals() public view returns (uint256 decimals)",
+  "function symbol() public view returns (string symbol)",
+];
 
-const TEST_ABI = [
-    "function updateValue(uint val) external"
-]
+const TEST_ABI = ["function updateValue(uint val) external"];
 
-const gelato = new GelatoRelay()
+const gelato = new GelatoRelay();
 
 export interface TokenInfo {
-    address: string,
-    symbol: string, 
-    decimals: bigint 
+  address: string;
+  symbol: string;
+  decimals: bigint;
 }
 
-export const isKnownSamplePlugin = (chainId: number, address: string): boolean => 
-    ethers.toBigInt(chainId) == ethers.toBigInt(SAMPLE_PLUGIN_CHAIN_ID) &&
-    getAddress(address) === SAMPLE_PLUGIN_ADDRESS  
+export const isKnownSamplePlugin = (
+  chainId: number,
+  address: string
+): boolean =>
+  ethers.toBigInt(chainId) == ethers.toBigInt(SAMPLE_PLUGIN_CHAIN_ID) &&
+  getAddress(address) === SAMPLE_PLUGIN_ADDRESS;
 
-const getRelayPlugin = async() => {
-    const provider = await getProvider()
-    return new ethers.Contract(
-        SAMPLE_PLUGIN_ADDRESS,
-        SAMPLE_PLUGIN_ABI,
-        provider
-    )
-}
+const getRelayPlugin = async () => {
+  const provider = await getProvider();
+  return new ethers.Contract(
+    SAMPLE_PLUGIN_ADDRESS,
+    SAMPLE_PLUGIN_ABI,
+    provider
+  );
+};
 
-const getTest = async() => {
-    const provider = await getProvider()
-    return new ethers.Contract(
-        "0xd8af3FE1314d5E8A1f2B0292521745b44Ec0DA59",
-        TEST_ABI,
-        provider
-    )
-}
+const getTest = async () => {
+  const provider = await getProvider();
+  return new ethers.Contract(
+    "0xd8af3FE1314d5E8A1f2B0292521745b44Ec0DA59",
+    TEST_ABI,
+    provider
+  );
+};
 
-const getToken = async(address: string) => {
-    const provider = await getProvider()
-    return new ethers.Contract(
-        address,
-        ECR20_ABI,
-        provider
-    )
-}
+const getToken = async (address: string) => {
+  const provider = await getProvider();
+  return new ethers.Contract(address, ECR20_ABI, provider);
+};
 
-export const getNextTxs = async(safe: string): Promise<SafeMultisigTransaction[]> => {
-    const currentNonce = await getCurrentNonce(safe)
-    const { results: txs } = await getSafeMultisigTxs(safe, { nonce: currentNonce })
-    return txs
-}
+export const getNextTxs = async (
+  safe: string
+): Promise<SafeMultisigTransaction[]> => {
+  const currentNonce = await getCurrentNonce(safe);
+  const { results: txs } = await getSafeMultisigTxs(safe, {
+    nonce: currentNonce,
+  });
+  return txs;
+};
 
-export const getAvailableFeeToken = async(): Promise<string[]> => {
-    return await gelato.getPaymentTokens(SAMPLE_PLUGIN_CHAIN_ID)
-}
+export const getAvailableFeeToken = async (): Promise<string[]> => {
+  return await gelato.getPaymentTokens(SAMPLE_PLUGIN_CHAIN_ID);
+};
 
-export const getMaxFeePerToken = async(account: string, token: string): Promise<bigint> => {
-    const plugin = await getRelayPlugin()
-    return await plugin.maxFeePerToken(account, token)
-}
+export const getMaxFeePerToken = async (
+  account: string,
+  token: string
+): Promise<bigint> => {
+  const plugin = await getRelayPlugin();
+  return await plugin.maxFeePerToken(account, token);
+};
 
-export const updateMaxFeePerToken = async(token: string, maxFee: bigint) => {
-    try {
-        const plugin = await getRelayPlugin()
-        await submitTxs([
-            {
-                to: await plugin.getAddress(),
-                value: "0",
-                data: (await plugin.setMaxFeePerToken.populateTransaction(token, maxFee)).data
-            }
-        ])
-    } catch (e) {
-        console.error(e)
-    }
-}
+export const updateMaxFeePerToken = async (token: string, maxFee: bigint) => {
+  try {
+    const plugin = await getRelayPlugin();
+    await submitTxs([
+      {
+        to: await plugin.getAddress(),
+        value: "0",
+        data: (
+          await plugin.setMaxFeePerToken.populateTransaction(token, maxFee)
+        ).data,
+      },
+    ]);
+  } catch (e) {
+    console.error(e);
+  }
+};
 
-export const getTokenInfo = async(address: string): Promise<TokenInfo> => {
-    if (address === NATIVE_TOKEN || address === ZeroAddress) return {
-        address,
-        symbol: "ETH",
-        decimals: BigInt(18)
-    }
-    const token = await getToken(address)
+export const getTokenInfo = async (address: string): Promise<TokenInfo> => {
+  if (address === NATIVE_TOKEN || address === ZeroAddress)
     return {
-        address,
-        symbol: await token.symbol(),
-        decimals: await token.decimals()
-    } 
-}
+      address,
+      symbol: "ETH",
+      decimals: BigInt(18),
+    };
+  const token = await getToken(address);
+  return {
+    address,
+    symbol: await token.symbol(),
+    decimals: await token.decimals(),
+  };
+};
 
-export const getTransaction = async() => {
-    const to = "0xd8af3FE1314d5E8A1f2B0292521745b44Ec0DA59"
-    const test = await getTest()
-    const data1 = (await test.updateValue.populateTransaction(10)).data
-    const actions = []
-    actions.push([to, 0, data1])
-    return [actions, 13, "0x0000000000000000000000000000000000000000000000000000000000000000"]
-}
+export const getTransaction = async () => {
+  const to = "0xd8af3FE1314d5E8A1f2B0292521745b44Ec0DA59";
+  const test = await getTest();
+  const data1 = (await test.updateValue.populateTransaction(BigInt(10))).data;
+  const actions = [];
+  actions.push({ to: to, value: 0, data: data1 });
+  return {
+    actions: actions,
+    nonce: 15,
+    metadataHash:
+      "0x0000000000000000000000000000000000000000000000000000000000000000",
+  };
+};
 
-export const relayTx = async(account: string, feeToken: string) => {
-    try {
-        const tx = await getTransaction()
-        const plugin = await getRelayPlugin()
-        const manager = await getManager()
+export const relayTx = async (account: string, feeToken: string) => {
+  try {
+    const tx = await getTransaction();
+    const plugin = await getRelayPlugin();
+    const manager = await getManager();
 
-        const request = {
-            chainId: SAMPLE_PLUGIN_CHAIN_ID,
-            target: await plugin.getAddress(),
-            data: (await plugin.executeFromPlugin.populateTransaction(await manager.getAddress(), account, tx)).data,
-            feeToken,
-            isRelayContext: true
-        }
-        console.log({request})
-        const response = await gelato.callWithSyncFee(request)
-        console.log(response)
-        return response.taskId
-    } catch (e) {
-        console.error(e)
-        return ""
-    }
-}
+    const request = {
+      chainId: SAMPLE_PLUGIN_CHAIN_ID,
+      target: await plugin.getAddress(),
+      data: (
+        await plugin.executeFromPlugin.populateTransaction(
+          await manager.getAddress(),
+          account,
+          tx
+        )
+      ).data,
+      feeToken,
+      isRelayContext: true,
+    };
+    console.log({ request });
+    const response = await gelato.callWithSyncFee(request);
+    console.log(response);
+    return response.taskId;
+  } catch (e) {
+    console.error(e);
+    return "";
+  }
+};
 
-export const getStatus = async(taskId: string) => {
-    try {
-        return await gelato.getTaskStatus(taskId)
-    } catch (e) {
-        console.error(e)
-        return undefined
-    }
-}
+export const getStatus = async (taskId: string) => {
+  try {
+    return await gelato.getTaskStatus(taskId);
+  } catch (e) {
+    console.error(e);
+    return undefined;
+  }
+};
